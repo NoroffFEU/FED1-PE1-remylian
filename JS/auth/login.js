6d3cf63326804d7b2eb597e935218107adaecc20
remylian <137064504+remylian@users.noreply.github.com>
2025-02-22 14:24:32 +0100

    the pathing issues are plentyful!

--- JS/auth/login.js
+++ JS/auth/login.js
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', () => {
 			localStorage.setItem('username', userData.name);
 			console.log(userData.name);
 
-			window.location.href = '../index.html';
+			window.location.href = 'index.html';
 		}, errorMessageDiv)
 	);
 });
